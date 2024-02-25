6b58ffd372519759f49f19ad7fc81452350c36d2
Bernardo Ralao <bernralao@gmail.com>
2024-02-25 21:57:02 +0000

    a

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Admin from './pages/Admin/Admin'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Admin/>
     </div>
   )
 }
